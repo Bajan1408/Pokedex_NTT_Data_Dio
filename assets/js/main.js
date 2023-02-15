a3d4ade35d1da3ecdf29d936ded3510987d04b33
Bajan1408 <gianrodrigues@gmail.com>
2023-02-15 16:43:53 -0300

    Criação do projeto e da página inicial com os pokemons

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const loadLessButton = document.getElementById('loadLessButton');
+let offset = 0;
+const limit = 10;
+const maxRecords = 151;
+
+function convertPokemonToLi(pokemon){
+   return `<a class="linkBig" href="/bigPokemon.html?id=${pokemon.number}">
+   <li class="pokemon ${pokemon.type}">
+   <span class="number">#00${pokemon.number}</span>
+   <span class="name">${pokemon.name}</span>
+   
+   <div class="detail">
+     <ol class="types">
+       <!--<li class="type">grass</li>
+       <li class="type">poison</li> -->
+       ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>` ).join('')}
+     </ol>
+
+    <!-- <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+     alt="${pokemon.name}"> -->
+
+    <img src="${pokemon.photo}" 
+    alt="${pokemon.name}">
+   </div>    
+ </li>
+   `
+}
+
+   
+function loadPokemonItems(offset, limit){
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {    
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+  })
+.catch((error) => console.log(`Este foi o meu erro: ${error}`))
+}
+
+loadPokemonItems(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+  offset += limit;
+  console.log('ofset + limit dentro do loadmorebutton valor ', offset + limit);
+  if((offset + limit) >= maxRecords){
+    const newLimit = maxRecords - offset;
+    loadPokemonItems(offset, newLimit);
+    loadMoreButton.parentNode.removeChild(loadMoreButton);
+  } 
+  else {
+    loadPokemonItems(offset, limit);
+     }
+  })
+
+ loadLessButton.addEventListener('window.load', () => {
+    if((offset + limit) > 10){
+      console.log('Tem algum erro aqui??1');
+    //loadLessButton.parentNode.removeChild(loadLessButton);
+    document.getElementById('loadLessButton').remove();
+    console.log('Tem algum erro aqui??');
+   }
+  })
+
+  console.log(offset + limit);
+  
+ 
